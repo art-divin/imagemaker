b792aeb46055620846b97332e5aeab9bf1af3c5a
Ruslan Alikhamov <r.alikhamov@gmail.com>
2019-02-27 10:23:39 +0300

    basic implementation of Locationer

---
+++ Locationer/Locationer/Locationer.swift
@@ -0,0 +1,51 @@
+//
+//  Locationer.swift
+//  Locationer
+//
+//  Created by Ruslan Alikhamov on 27.02.19.
+//  Copyright © 2019 Imagemaker. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+public class Locationer : NSObject {
+    
+    fileprivate var callback : ((CLLocationCoordinate2D?, Error?) -> Void)
+    
+    init(callback: @escaping (CLLocationCoordinate2D?, Error?) -> Void) {
+        self.callback = callback
+    }
+    
+    lazy var locationManager : CLLocationManager = {
+        let retVal = CLLocationManager()
+        retVal.activityType = .fitness
+        retVal.allowsBackgroundLocationUpdates = true
+        retVal.delegate = self
+        retVal.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        retVal.requestAlwaysAuthorization()
+        return retVal
+    }()
+    
+}
+
+extension Locationer : CLLocationManagerDelegate {
+    
+    public func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status != .authorizedAlways {
+            fatalError("unable to work with such authorization")
+        }
+        manager.startUpdatingLocation()
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let mostRecent = locations.last
+        self.callback(mostRecent?.coordinate, nil)
+    }
+    
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        self.callback(nil, error)
+        fatalError("failed to fetch location! \(error)")
+    }
+    
+}
